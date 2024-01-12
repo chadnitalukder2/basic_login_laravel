9f7752700212b14344106999fba825fc57224e79
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2024-01-12 23:10:36 +0600

    breeze-login-com

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -18,11 +18,11 @@
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
         
-
         <div class="flex justify-end mt-4">
             <x-primary-button>
                 {{ __('Confirm') }}
             </x-primary-button>
         </div>
     </form>
+    
 </x-guest-layout>
